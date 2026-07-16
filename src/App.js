7d02ec9fdb147815123c2c3a5870bcda46b60286
AntonioFloresLara259 <antonio@engranajesciencia.com>
2026-07-16 09:35:38 +0200

    refactor: restructure app components and remove unused files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Sunglasses", quantity: 1, packed: false },
+  { id: 3, description: "Water Bottle", quantity: 1, packed: false },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Starts />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1> 🏝 Far Away 💼</h1>;
+}
+
+function Form() {
+  function handeleSubmit(e) {
+    e.prevebtDefault();
+  }
+  return (
+    <form className="add-form" onSubmit={(e) => handeleSubmit(e)}>
+      <h3>What do you need for your trip?💖</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {" "}
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => {}}>❌</button>
+    </li>
+  );
+}
+
+function Starts() {
+  return (
+    <footer className="stats">
+      <em>
+        💼 You have 0 items on your packing list, and you already packed 0 (0%)
+      </em>
+    </footer>
+  );
+}
